550c68b061e087eb1c204a0ba9e5bb4f3545dbd3
Myung Joon Kang <contact@myungjoon.com>
2026-01-17 17:00:08 -0800

    Remove unused imports and rename "getCurrentDrive" method to "getCurrentSwerveRequest"

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,7 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
-import java.security.Guard;
 import java.util.List;
-
-import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
@@ -18,11 +15,9 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -58,7 +53,7 @@ public class RobotContainer {
         m_drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             m_drivetrain.applyRequest(() ->
-                getCurrentDrive()
+                getCurrentSwerveRequest()
             )
         );
 
@@ -118,7 +113,7 @@ public class RobotContainer {
         }
     }
 
-    public SwerveRequest.FieldCentricFacingAngle getCurrentDrive() {
+    public SwerveRequest.FieldCentricFacingAngle getCurrentSwerveRequest() {
         if (isSnapToggleOn) {
             return m_drive
                 .withVelocityX(-m_controller.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
