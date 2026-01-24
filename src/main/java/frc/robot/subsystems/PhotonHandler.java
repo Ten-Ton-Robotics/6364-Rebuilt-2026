f57d18673835b8cc39fea6b841cc10218f17496f
Myung Joon Kang <contact@myungjoon.com>
2026-01-23 20:10:01 -0800

    create doesTagMatchAlliance method

--- src/main/java/frc/robot/subsystems/PhotonHandler.java
+++ src/main/java/frc/robot/subsystems/PhotonHandler.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
  
 import java.lang.String;
+import java.lang.StackWalker.Option;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
@@ -11,6 +12,8 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import frc.robot.Robot;
 
@@ -185,5 +188,22 @@ import org.photonvision.targeting.PhotonTrackedTarget;
      @FunctionalInterface
      public static interface EstimateConsumer {
          public void accept(Pose2d pose, double timestamp, Matrix<N3, N1> estimationStdDevs);
+     }
+
+     public boolean doesTagMatchAlliance(int id) {
+        final List<Integer> blueIds = List.of(17, 28, 18, 27, 19, 20, 26, 25, 21, 24, 22, 23, 29, 30, 31, 32);
+        final List<Integer> redIDs = List.of(7, 6, 8, 5, 9, 10, 4, 3, 11, 2, 12, 1, 16, 15, 14, 13);
+
+        Optional<Alliance> alliance = DriverStation.getAlliance();
+
+        if (alliance.isPresent()) {
+            if (alliance.get() == DriverStation.Alliance.Blue) {
+                return blueIds.contains(id);
+            } else {
+                return redIDs.contains(id);
+            }
+        } else {
+            return false;
+        }
      }
  }
