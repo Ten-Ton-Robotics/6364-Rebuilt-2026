8d90fb87720174c7ba60ea81d822821032b3f22f
Myung Joon Kang <contact@myungjoon.com>
2026-01-23 18:57:45 -0800

    .

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -22,6 +22,12 @@ public class Shooter extends SubsystemBase {
     public static final double kKP = 0.35;
     public static final double kKI = 0;
     public static final double kKD = 0;
+    public static final double kKS = 0;
+    public static final double kKV = 0;
+    public static final double kKA = 0;
+
+    // Drive Ratio
+    public static final double kRatio = 1;
 
     // Motor
     private final TalonFX m_motor = new TalonFX(kMotorID, kMotorBus);
@@ -36,7 +42,10 @@ public class Shooter extends SubsystemBase {
         final TalonFXConfiguration config = new TalonFXConfiguration();
 
         // set controller gains
-        config.Slot0 = new Slot0Configs().withKP(kKP).withKI(kKI).withKD(kKD);
+        config.Slot0 = new Slot0Configs().withKP(kKP).withKI(kKI).withKD(kKD).withKS(kKS).withKV(kKV).withKA(kKA);
+
+        // set ratio
+        config.Feedback.SensorToMechanismRatio = kRatio;
 
         // apply configuration
         m_motor.getConfigurator().apply(config);
