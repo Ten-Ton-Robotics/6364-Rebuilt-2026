f63be3e80a2f99c8f37d21b08c6aaa44d06db8d8
Myung Joon Kang <contact@myungjoon.com>
2026-01-23 20:11:29 -0800

    Tweaking...

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.CANBus;
-import com.ctre.phoenix6.configs.Slot0Configs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.StaticBrake;
 import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -18,38 +16,11 @@ public class Shooter extends SubsystemBase {
     public static final int kMotorID = 21;
     public static final int kSpeed = 80;
 
-    // Controller gains
-    public static final double kKP = 0.35;
-    public static final double kKI = 0;
-    public static final double kKD = 0;
-    public static final double kKS = 0;
-    public static final double kKV = 0;
-    public static final double kKA = 0;
-
-    // Drive Ratio
-    public static final double kRatio = 1;
-
     // Motor
     private final TalonFX m_motor = new TalonFX(kMotorID, kMotorBus);
 
     // Motor Output
     private final VelocityTorqueCurrentFOC m_output = new VelocityTorqueCurrentFOC(kSpeed);
-
-    public Shooter() {
-        super();
-
-        // configure motor
-        final TalonFXConfiguration config = new TalonFXConfiguration();
-
-        // set controller gains
-        config.Slot0 = new Slot0Configs().withKP(kKP).withKI(kKI).withKD(kKD).withKS(kKS).withKV(kKV).withKA(kKA);
-
-        // set ratio
-        config.Feedback.SensorToMechanismRatio = kRatio;
-
-        // apply configuration
-        m_motor.getConfigurator().apply(config);
-    }
 
     // Commands
     public Command shoot() {
