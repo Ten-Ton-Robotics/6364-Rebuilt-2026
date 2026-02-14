9a36b85c00dba791e36d56dacad0950e64ca3675
Myung Joon Kang <contact@myungjoon.com>
2026-02-13 20:29:31 -0800

    First prototype for hub snapping toggle

---
+++ src/main/java/frc/robot/FieldConstants.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+import java.util.Optional;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+public class FieldConstants {
+    public static final Translation2d kBlueHub = new Translation2d(11.915394, 4.034536);
+    public static final Translation2d kRedHub = new Translation2d(4.625594, 4.034536);
+
+    public static Translation2d getHubPositionMatchingAlliance() {
+        Optional<Alliance> alliance = DriverStation.getAlliance();
+
+        if (alliance.isPresent()) {
+            return alliance.get() == Alliance.Blue ? kBlueHub : kRedHub;
+        } else {
+            System.out.println("FieldConstants tried to access Alliance but failed; returning Blue by default.");
+            return kBlueHub;
+        }
+    }
+}
