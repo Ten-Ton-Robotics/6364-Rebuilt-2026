0b7a7b52ff94e15dd7f507f935cb7c6dadc572ad
Aaron Ashclarke <aaron.ashclarke@gmail.com>
2026-03-01 17:45:59 -0800

    Camera Constants

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -21,7 +21,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   public static PhotonHandler m_vision_top;
-  // public static PhotonHandler m_vision_back;
+  public static PhotonHandler m_vision_back;
 
   private final RobotContainer m_robotContainer;
 
@@ -31,8 +31,8 @@ public class Robot extends TimedRobot {
     Transform3d cameraToRobotForTopCamera = new Transform3d(new Translation3d(-0.06, 0.11, 0.738), new Rotation3d(0, 0.5759586532, Math.PI));
     m_vision_top = new PhotonHandler(drivetrain::addVisionMeasurement, "TopCam" , cameraToRobotForTopCamera.inverse());
 
-    // Transform3d cameraToRobotForBackCamera = new Transform3d(new Translation3d(-0.2286, 0.2794, 0.61876), new Rotation3d(0, 0, Math.PI));
-    // m_vision_back = new PhotonHandler(drivetrain::addVisionMeasurement, "SideCam" , cameraToRobotForBackCamera.inverse());
+    Transform3d cameraToRobotForBackCamera = new Transform3d(new Translation3d(-0.038, 0, 0.703), new Rotation3d(0, -0.9, 0));
+    m_vision_back = new PhotonHandler(drivetrain::addVisionMeasurement, "SideCam" , cameraToRobotForBackCamera.inverse());
 
     // CameraServer.startAutomaticCapture();
   }
