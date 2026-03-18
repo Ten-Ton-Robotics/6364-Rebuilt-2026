98a17974b418668792ef9c54a98154085832f4be
Aaron Ashclarke <aaron.ashclarke@gmail.com>
2026-03-17 18:00:19 -0700

    Motor Speed and Amp Fix

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -22,8 +22,8 @@ public class Intake extends SubsystemBase {
     private static final CANBus kMotorBus = new CANBus("CANCAN");
 
     private static final int kMotorID = 34; //Get motor ID from TunerX put that one here
-    private static double TargetSpeed = 50; 
-    private static double MaxSpeed = 50;
+    private static double TargetSpeed = 75; 
+    private static double MaxSpeed = 75;
 
     // Motor
     private final TalonFX m_motor = new TalonFX(kMotorID, kMotorBus);
@@ -46,7 +46,7 @@ public class Intake extends SubsystemBase {
         var motorConfig = new TalonFXConfiguration()
         .withCurrentLimits(
                 new CurrentLimitsConfigs()
-                    .withStatorCurrentLimit(Amps.of(40))
+                    .withStatorCurrentLimit(Amps.of(45))
                     .withStatorCurrentLimitEnable(true)
             )
         .withSlot0(slot0Configs)
