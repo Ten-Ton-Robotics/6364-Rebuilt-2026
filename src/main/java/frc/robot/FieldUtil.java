6fbdfab1bd5703b4fcb039efe0a8a1ba627d9e97
Aaron Ashclarke <aaron.ashclarke@gmail.com>
2026-04-03 21:39:52 -0700

    Undo Classroom Changes

--- src/main/java/frc/robot/FieldUtil.java
+++ src/main/java/frc/robot/FieldUtil.java
@@ -10,12 +10,12 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class FieldUtil {
-    // public static final Translation2d kBlueHub = new Translation2d(4.6, 4.03);
-    // public static final Translation2d kRedHub = new Translation2d(11.915394, 4.034536);
+    public static final Translation2d kBlueHub = new Translation2d(4.6, 4.03);
+    public static final Translation2d kRedHub = new Translation2d(11.915394, 4.034536);
     
     //Classroom SETTINGS
-    public static final Translation2d kBlueHub = new Translation2d(11.915394, 4.034536);
-    public static final Translation2d kRedHub = new Translation2d(4.6, 4.03);
+    // public static final Translation2d kBlueHub = new Translation2d(11.915394, 4.034536);
+    // public static final Translation2d kRedHub = new Translation2d(4.6, 4.03);
 
     private static TreeMap<Double, Double> rangeMap = new TreeMap<>();   
     
